a3eff35643ba0f37ea007f22861c8b9428971aec
Muhammed Thaha <mthaha1989@gmail.com>
2014-04-01 11:23:25 +0530

    Update hello.rb

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,4 +2,5 @@ require 'greeter'
 # Default is "World"
 # Author: Muhammed Thaha(mthaha1989@gmail.com)
 name = ARGV.first || "World"
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
